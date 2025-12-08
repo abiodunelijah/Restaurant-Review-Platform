df0121a1b70281539f0686ae6d482184c808a3b7
sir-abbey <kaybiel4u@gmail.com>
2025-12-09 00:06:22 +0100

    Initial commit

---
+++ restaurant/src/main/java/com/coder2client/entities/User.java
@@ -0,0 +1,26 @@
+package com.coder2client.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String username;
+
+    @Field(type = FieldType.Text)
+    private String givenName;
+
+    @Field(type = FieldType.Text)
+    private String familyName;
+}
